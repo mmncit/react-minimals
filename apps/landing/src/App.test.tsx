9e611fb7022e2533baffc45951fb55d79304ff83
Mamun <mmncit@gmail.com>
2023-02-27 00:20:34 -0700

    fix test

--- apps/landing/src/App.test.tsx
+++ apps/landing/src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+function mountComponent() {
+  return render(<App />);
+}
+
+test("renders div", () => {
+  mountComponent();
+  expect(
+    screen.queryByText("the snozzberries taste like snozzberries")
+  ).toBeTruthy();
 });
--- apps/landing/src/App.tsx
+++ apps/landing/src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import "./App.css";
 import { Thing } from "@shared/components";
 
 function App() {
